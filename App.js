baf2f4e5f54ce65e67a495c29a8e05a2f9815968
waultinterview <Wilson.chee@wiztechww.com>
2026-02-25 14:00:01 +0800

    Update App.js

--- App.js
+++ App.js
@@ -1,24 +1,27 @@
 function CheckDiscount ({totalAmount, discountrate}) {   				
 	const handleClick = () => {
-		totalAmount = totalAmount * discountrate;
+		totalAmount = totalAmount - (totalAmount * discountrate);
 	};
 	return(
-		<><button onClick={handleClick}>Calc after Discount (10% discount)</button></>        
+		<><button onClick={handleClick}>Calc Final Total</button></>        
 	);
 }
 
 export default function App() {
+ 	const styleObj = {
+    	textAlign: 'center',      
+    	marginTop: '50px'
+  	};
 	let totalAmount = 1000;
-	let discountrate = 1;      
+	let discountrate = 0;      
 	if(totalAmount > 500){
-		discountrate = 0.9;
+		discountrate = 10;
 	}     
 	return (
-		<div>
-			<div>Original Total: 1000</div>
+		<div style={styleObj}>
+			<div>Original Total: 1000, discount rate: {discountrate}%</div>
 			<CheckDiscount totalAmount={totalAmount} discountrate={discountrate}/>
 			<div>Final Total: {totalAmount.toFixed(2)}</div>
 		</div>
 	);
 }
-
